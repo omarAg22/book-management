ca40950f666dcdb9d24bdb4a0077332618a8668f
Omar AGTIB <22400016@moss.center>
2025-07-11 17:34:58 +0100

    commiting frontend --1

---
+++ mobile/app/index.jsx
@@ -0,0 +1,25 @@
+import { Link } from "expo-router";
+import { Text, View, StyleSheet } from "react-native";
+
+export default function Index() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Hello</Text>
+
+      <Link href="/(auth)/signup">Sign Up</Link>
+      <Link href="/(auth)">Login</Link>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  text: {
+    fontSize: 20,
+    color: "blue",
+  },
+});
